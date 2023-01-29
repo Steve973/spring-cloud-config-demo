adcf8ad15e515b9b8d571d873ecbdd55bbb59ac5
Steve Storck <steve973@gmail.com>
2023-01-29 14:38:54 -0500

    Added spring cloud bus dependencies for config update notifications from git, but have not yet added configuration to make it operational.

--- config-client/src/main/java/com/example/configclient/web/ConfigClientController.java
+++ config-client/src/main/java/com/example/configclient/web/ConfigClientController.java
@@ -1,11 +1,13 @@
 package com.example.configclient.web;
 
 import org.bson.Document;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     final String mainPropertyA;
